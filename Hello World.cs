257c01760aa1b50e0938ebd4cb74cbec8679d05d
RamiasSierr4 <64395903+RamiasSierr4@users.noreply.github.com>
2020-04-30 21:51:29 +0200

    Initial commit

---
+++ Hello World.cs
@@ -0,0 +1,12 @@
+namespace HelloWorld
+{
+    class Program
+    {
+        static void main(string[]args)
+        {
+            Console.Writline("Hello World");
+            Console.ReadLine();
+            
+        }
+    }
+}
